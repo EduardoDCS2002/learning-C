c7e7fd8c322a7c0e6093f4b69ce7c900f67de075
EduDCS <77997232+EduardoDCS2002@users.noreply.github.com>
2021-03-22 18:06:28 +0000

    Update triangulos.c

--- triangulos.c
+++ triangulos.c
@@ -2,41 +2,51 @@
 #include <stdio.h>
 #include <assert.h>
 
-int main() {
+/*
+aqui ao fornecer 3 números inteiros que são 3 lados de um triangulo e vai dizer se é inválido ou, se for válido, 
+vai dizer que tipo de triângulo é seguido do perímetro e da área
+*/
+int main()
+{
     int a, b, c;
-    assert(scanf("%d%d%d", &a, &b, &c)==3);
-     double perimetro = (a+b+c);
-     double p = (perimetro)/2;
-     double area = sqrt(p*(p-a)*(p-b)*(p-c));
+    assert(scanf("%d%d%d", &a, &b, &c) == 3);
+    double perimetro = (a + b + c);
+    double p = (perimetro) / 2;
+    double area = sqrt(p * (p - a) * (p - b) * (p - c));
 
-    if (( (a+b) <= c) || ( (a+c) <= b) || ( (b+c) <= a)) {
-        
+    if (((a + b) <= c) || ((a + c) <= b) || ((b + c) <= a))
+    {
+
         printf("INVALIDO\n");
         return 0;
     }
-    if(((a*a)+(b*b)==(c*c)) || ((a*a)+(c*c)==(b*b)) || ((c*c)+(b*b)==(a*a))){
+    if (((a * a) + (b * b) == (c * c)) || ((a * a) + (c * c) == (b * b)) || ((c * c) + (b * b) == (a * a)))
+    {
 
         printf("RETANGULO %.0f %.2f\n", perimetro, area);
-        
+
         return 0;
     }
-    if (a==b && a==c && b==c){
-        
+    if (a == b && a == c && b == c)
+    {
+
         printf("EQUILATERO %.0f %.2f\n", perimetro, area);
 
         return 0;
     }
-    if (a==b || b==c || a==c){
+    if (a == b || b == c || a == c)
+    {
 
         printf("ISOSCELES %.0f %.2f\n", perimetro, area);
 
         return 0;
     }
-    if (a!=b && a!=c && b!=c){
+    if (a != b && a != c && b != c)
+    {
         printf("ESCALENO %.0f %.2f\n", perimetro, area);
 
         return 0;
     }
-    
+
     return 0;
 }
